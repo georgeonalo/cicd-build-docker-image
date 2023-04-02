da6c02715c5fed4c6ff4bdd6f5f0765f95df72c0
George Onalo <onalotech7@gmail.com>
2023-04-02 21:36:23 +0100

    update files

--- push-image.sh
+++ push-image.sh
@@ -4,10 +4,10 @@
 set -eu
 
 # login to your docker hub account
-docker login --username $DOCKER_HUB_USERNAME --password  $DOCKER_HUB_PASSWORD
+docker login --username $DOCKER_HUB_USERNAME --password $DOCKER_HUB_PASSWORD
 
 # use the docker tag command to give the image a new name
-sudo docker tag $IMAGE_TAG $DOCKER_HUB_USERNAME/$DOCKER_HUB_REPO_NAME
+docker tag $IMAGE_TAG $DOCKER_HUB_USERNAME/$DOCKER_HUB_REPO_NAME
 
 # push the image to your docker hub repository
 docker push  $DOCKER_HUB_USERNAME/$DOCKER_HUB_REPO_NAME
